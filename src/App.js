0e6954141127c312f30013b491ed16e01f24c2d8
Ian Li <ian.li1023@gmail.com>
2022-03-18 14:31:23 -0600

    upladed photos, created navbar/header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+import MainContent from "./components/Header/Header";
+
 function App() {
   return (
     <div >
-      
+      <Navbar/>
+      <MainContent/>
     </div>
   );
 }
