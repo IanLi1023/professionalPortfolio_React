189a151302b30684c13b681f61c167d7e0e690d7
Ian Li <ian.li1023@gmail.com>
2022-03-18 17:22:21 -0600

    created navbar functions and PortfolioContainer file/function

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import '../Navbar/Navbar.css';
 
-function Navbar() {
+function Navbar({ currentPage, handlePageChange }) {
     return (
         <header>
             <nav>
                 <a href="#IanLi">Ian Li</a>
-                <a href="#about">About Me</a>
-                <a href="#work">My Work</a>
-                <a href="#contact">Contact Me</a>
+                <a href="#about"
+                onClick={() => handlePageChange('Aboutme')}      
+                className={currentPage === 'Aboutme' ? 'nav-link active' : 'nav-link'}>About Me</a>
+                
+                <a href="#work"
+                onClick={() => handlePageChange('Work')}      
+                className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
+                >My Work</a>
+                
+                <a href="#contact"
+                onClick={() => handlePageChange('Contact')}      
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >Contact Me</a>
             </nav>
         </header>
     );
