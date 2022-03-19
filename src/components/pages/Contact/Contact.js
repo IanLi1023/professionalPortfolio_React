e6d1ace7a093bb92ae67bdc013e7864a3fb0f9aa
Ian Li <ian.li1023@gmail.com>
2022-03-19 14:50:12 -0600

    added icons to contact me page

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import './Contact.css';
+
 
 function ContactMe() {
     return (
@@ -10,6 +15,7 @@ function ContactMe() {
                 <article className="contact-div" id="email">
                     <a className="contact-icon" href="mailto:Ian.Li1023@gmail.com">
                         <i className="far fa-envelope fa-2x"></i>
+                        <MailOutlineIcon sx={{ fontSize: "4rem" }}/>
                         <p className="contact-text">Email</p>
                     </a>
                 </article>
@@ -17,21 +23,23 @@ function ContactMe() {
                 <article className="contact-div" id="phone">
                     <a className="contact-icon" href="tel:303.564.0668">
                         <i className="fas fa-phone fa-2x"></i>
+                        <LocalPhoneIcon sx={{ fontSize: "4rem" }}/>
                         <p className="contact-text">Phone</p>
                     </a>
                 </article>
 
                 <article className="contact-div" id="linkedin">
                     <a className="contact-icon" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/ian-li1023/">
-                        <i className="fab fa-linkedin-in fa-2x"></i>
+                        <LinkedInIcon sx={{ fontSize: "4rem" }}/>
                         <p className="contact-text">LinkedIn</p>
                     </a>
                 </article>
 
                 <article className="contact-div" id="github">
-                    <a className="contact-icon" target="_blanks" rel="noreferrer" href="https://github.com/IanLi1023">
-                        <i className="fab fa-github fa-2x"></i>
+                    <a className="contact-icon" target="_blank" rel="noreferrer" href="https://github.com/IanLi1023">
+                        <GitHubIcon sx={{ fontSize: "4rem" }}/>
                         <p className="contact-text">GitHub</p>
+                       
                     </a>
                 </article>
             </div>
