e268207d2176ba3f26824fd9eacaa3f87622745b
Ian Li <ian.li1023@gmail.com>
2022-03-20 22:48:57 -0600

    fixed css on mywork

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,10 @@ function Navbar({ currentPage, handlePageChange }) {
     return (
         <header>
             <nav>
-                <a href="#IanLi">Ian Li</a>
+                <a href="#IanLi"
+                onClick={() => handlePageChange('IanLi')}      
+                className={currentPage === 'Aboutme' ? 'nav-link active' : 'nav-link'}>Ian Li</a>
+
                 <a href="#about"
                 onClick={() => handlePageChange('Aboutme')}      
                 className={currentPage === 'Aboutme' ? 'nav-link active' : 'nav-link'}>About Me</a>
